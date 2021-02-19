7bd0cfb55a12eae243597ed696f36a6d4f5a7547
Kshitij7421 <71764916+Kshitij7421@users.noreply.github.com>
2021-02-19 15:30:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+song = "" ;
+song2 = "" ;
+
+function preload() {
+    song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3")
+}
+
+function setup() {
+    canvas = createCanvas(600, 500)
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded) 
+}
